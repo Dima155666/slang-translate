64141694b670d410ca4fc90522e046bd6be53bc7
Dima155666 <Luizazukova1@gmail.com>
2024-09-28 17:19:12 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+words = {
+    'whats up mate': 'means in a friendly way',
+    'wanna go': 'means wanna fight or hangout',
+    'mate your a six head': 'means insulting your forehead',
+
+    words = input('Which word need translating?')
+    words = word.lower
+if word in words:
+    print('your translating')
+else:
+    print ('This word is not in a dictionary!') 
+    
